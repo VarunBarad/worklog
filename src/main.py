0e7661277c26100de0f7d196d57f0d67c7ac92f0
Varun Barad <varun@varunbarad.com>
2024-02-09 00:30:43 +0530

    Add a python script to append entry to our worklog file

---
+++ src/main.py
@@ -0,0 +1,34 @@
+import csv
+import datetime
+
+
+def main():
+    worklog_file = '/Users/varunb/worklog.csv'
+
+    worklog_entries = []
+    with open(worklog_file, 'r', newline='') as csvFile:
+        reader = csv.DictReader(csvFile)
+        worklog_entries.extend(reader)
+
+    print('Enter the new worklog entry')
+    new_entry = input()
+
+    now = datetime.datetime.now()
+    formatted_now = now.strftime('%Y-%m-%dT%H:%M+05:30')
+
+    worklog_entries.append({
+        'timestamp': formatted_now,
+        'contents': new_entry,
+    })
+
+    with open(worklog_file, 'w', newline='') as csvFile:
+        headers = ['timestamp', 'contents']
+        writer = csv.DictWriter(csvFile, fieldnames=headers)
+        writer.writeheader()
+        writer.writerows(worklog_entries)
+
+    print('Worklog updated!')
+
+
+if __name__ == '__main__':
+    main()
