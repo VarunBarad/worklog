63ab0ef2252f54b62f2e55f26d84ddbcb09e85da
Varun Barad <varun@varunbarad.com>
2024-03-04 12:10:49 +0530

    Provide functionality matching existing python version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use serde::{Deserialize, Serialize};
+use std::io::Write;
+
+#[derive(Debug, Deserialize, Serialize)]
+struct LogRecord {
+    timestamp: String,
+    contents: String,
+}
+
 fn main() {
-    println!("Hello, World!");
+    let mut records: Vec<LogRecord> = vec![];
+    {
+        let mut reader = csv::Reader::from_path("/Users/varunb/worklog.csv").unwrap();
+        for record in reader.deserialize() {
+            let record: LogRecord = record.unwrap();
+            records.push(record);
+        }
+    }
+
+    print!("Log entry: ");
+    std::io::stdout().flush().unwrap();
+    let mut contents: String = String::new();
+    std::io::stdin().read_line(&mut contents).unwrap();
+
+    let now = chrono::offset::Local::now();
+    records.push(LogRecord {
+        timestamp: now.format("%Y-%m-%dT%H:%M%:z").to_string(),
+        contents: contents.trim().to_owned(),
+    });
+
+    {
+        let mut writer = csv::Writer::from_path("/Users/varunb/worklog.csv").unwrap();
+        for record in records {
+            writer.serialize(record).unwrap();
+        }
+        writer.flush().unwrap();
+    }
 }
